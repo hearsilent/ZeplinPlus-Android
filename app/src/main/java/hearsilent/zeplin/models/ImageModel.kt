e74bf5aeb30fb1ad9e4f29f6ff0a640aa5d8e05f
HearSilent <hear.silent1995@gmail.com>
2020-11-02 17:23:13 +0800

    Fix parsing image model

--- app/src/main/java/hearsilent/zeplin/models/ImageModel.kt
+++ app/src/main/java/hearsilent/zeplin/models/ImageModel.kt
@@ -1,3 +1,6 @@
 package hearsilent.zeplin.models
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class ImageModel(val width: Int, val height: Int, val original_url: String)
